97096fc1ce89b4d09d77fbf0e47649005343d226
Grzegorz on Mac <grzegorz.zajac@me.com>
2021-08-23 23:22:24 +0200

    add mqtt test

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,28 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include "credentials.h"
+#include <PubSubClient.h>
 
 #define LED 2            // Led in NodeMCU at pin GPIO16 (D0). gpio2 ESP8266 led
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void callback(char* topic, byte* payload, unsigned int length) {
+
+  Serial.print("Message arrived in topic: ");
+  Serial.println(topic);
+
+  Serial.print("Message:");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+
+  Serial.println();
+  Serial.println("-----------------------");
+
+}
+
 void setup() {
 pinMode(LED, OUTPUT);    // LED pin as output.
 
@@ -21,6 +41,28 @@ Serial.println();
 
 Serial.print("Connected, IP address: ");
 Serial.println(WiFi.localIP());
+
+client.setServer(mqttServer, mqttPort);
+client.setCallback(callback);
+
+while (!client.connected()) {
+    Serial.println("Connecting to MQTT...");
+
+    if (client.connect("ESP8266Client")) {
+
+      Serial.println("connected");
+
+    } else {
+
+      Serial.print("failed with state ");
+      Serial.print(client.state());
+      delay(2000);
+
+    }
+  }
+
+  client.publish("esp/test", "Hello from ESP8266");
+  client.subscribe("esp/test");
 }
 
 void loop() {
@@ -29,4 +71,5 @@ digitalWrite(LED, HIGH);// turn the LED off.(Note that LOW is the voltage level 
 delay(1000);            // wait for 1 second.
 digitalWrite(LED, LOW); // turn the LED on.
 delay(1000); // wait for 1 second.
+client.publish("esp/test", "Hello from ESP8266");
 }
