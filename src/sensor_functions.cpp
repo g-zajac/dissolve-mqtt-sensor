15f5c782467a45548cca47c3cd3a43529c72ce31
Grzegorz on Mac <grzegorz.zajac@me.com>
2021-10-06 22:52:01 +0200

    add json

--- src/sensor_functions.cpp
+++ src/sensor_functions.cpp
@@ -1,24 +1,69 @@
 #include "sensor_functions.h"
 
 // class::constructor
-SnFn::SnFn(String sensor){
+SnFn::SnFn(String sensor, unsigned long sensorSamplingRate){
   _sensorType = sensor;
+  _sensorSampling = sensorSamplingRate;
   Serial.print("selected sensor: ");
   Serial.println(_sensorType);
+  Serial.print("sampling rate: ");
+  Serial.println(_sensorSampling);
+  _doc["type"] = sensor;
+  _doc["sampling"] = _sensorSampling;
+
+  const bool _useGyro = true;
+
+  #if (_useGyro)
+    Serial.println("====== Conditional lib adding success ========");
+    #include <Wire.h>
+    #include <L3G.h>
+    L3G gyro;
+
+    Wire.begin();
+    if (!gyro.init())
+    {
+      Serial.println("Failed to autodetect gyro type!");
+      while (1);
+    }
+    gyro.enableDefault();
+    Serial.println("gyro connected");
+  #endif
+
 }
 
+bool SnFn::read(){
+  #if (_useGyro)
+    gyro.read();
+
+    JsonArray data = _doc.createNestedArray("data");
+    data.add = (int)gyro.g.x;
+    data.add = (int)gyro.g.y;
+    data.add = (int)gyro.g.z;
+  #endif
+  return true;
+}
+
+char SnFn::data(){
+  char* out[128];
+  // out = "dupa jasiu";
+  int r = serializeJson(_doc, out);
+  return "dupa";
+}
 
 long SnFn::getRandomNumber(){
    unsigned long specialNumber = random(5,1000);
    return specialNumber;
 }
 
-String SnFn::sensorType(){
+String SnFn::type(){
   return _sensorType;
-  // return "dupa";
 }
 
-bool SnFn::sensorInit(){
+unsigned long SnFn::sampling(){
+  return _sensorSampling;
+}
+
+bool SnFn::initialise(){
   Serial.print("Initialising sensor: ");
   Serial.println(_sensorType);
   return true;
--- src/sensor_functions.h
+++ src/sensor_functions.h
@@ -3,15 +3,21 @@
 #define SF
 
 #include <Arduino.h>
+#include <ArduinoJson.h>
 
 // blue print for object
 class SnFn{                                     // accesible
   public:
     // Constructor with default value
-    SnFn(String sensorType="test");
+    SnFn(String sensorType="test", unsigned long samplingSensorRate=500);
 
-    String sensorType();
-    bool sensorInit();
+    String type();
+    unsigned long sampling();
+
+    bool initialise();
+    bool read();
+    char data();
+
     // Methods
     long getRandomNumber();
 
@@ -23,6 +29,10 @@ class SnFn{                                     // accesible
   // only in .cpp not in main sketch
   private:
     String _sensorType;
+    int _sensorSampling;
+    bool _useGyro;
+
+    StaticJsonDocument<256> _doc;
 };
 
 #endif
