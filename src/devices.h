14453a7d80962c256dc9e4ca0e16f430084a9296
Grzegorz on Mac <grzegorz.zajac@me.com>
2021-10-27 19:24:27 +0200

    update list with humidity sensors

--- src/devices.h
+++ src/devices.h
@@ -17,17 +17,17 @@ const device_details devices[] = {
   {0x00A97FCC, "008"},
   {0x0065EAC7, "009"},
   // ---------------------- OTHERS -----------------------------
-  {0x00666608, "010"},  // Temperature & Humidity
+  {0x00666608, "010"},  //
   {0x0066460B, "011"},  // Thermal camera
   {0x00A8F31B, "012"},  // Light RGB
   {0x00664624, "013"},  // Mic
-  {0x006648F0, "014"},
-  {0x00A91667, "015"},
+  {0x006648F0, "014"},  // Humidity
+  {0x00A91667, "015"},  // Humidity
   {0x0065AFF1, "016"},
   {0x006648F3, "017"},
   {0x00664552, "018"},
   {0x006643D4, "019"},
   {0x00664871, "020"},
   {0x0022AD43, "021"},  // Mobile Gyro on battery (Wemos D1)
-  {0x00, "099"} // default if not defined above
+  {0x00, "099"} // blank or dummy default if not defined above
 };
