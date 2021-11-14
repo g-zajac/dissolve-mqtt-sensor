0194c513b084430da50a4113b582a4a9150b902e
Grzegorz on Mac <grzegorz.zajac@me.com>
2021-11-14 23:00:10 +0100

    handover update

--- src/devices.h
+++ src/devices.h
@@ -13,24 +13,25 @@ const device_details devices[] = {
   {0x0066455C, "004"},  // proximity TOF0 - VL52L0X - 2m
   {0x0066425C, "005"},  // gesture/proximity/colour - RGB 16bit raw values
   {0x0065C28B, "006"},  // proximity ultrasonic SRF01
-  {0x0066465D, "007"},  // -- proximity, ultrasonic SFR02
-  {0x00A97FCC, "008"},  // -- proximity, ultrasonic HC-SR04
-  {0x0065EAC7, "009"},  //
+  {0x0066465D, "007"},  // proximity,ultrasonic SFR02
+  {0x00A97FCC, "008"},  // proximity,ultrasonic HC-SR04
+  {0x0065EAC7, "009"},  // air quality - CCS811 gas sensor
   // ---------------------- OTHERS -----------------------------
   {0x00666608, "010"},  // HR
-  {0x0066460B, "011"},  // Thermal camera
+  {0x0066460B, "011"},  // Thermal camera - 8x8
   {0x00A8F31B, "012"},  // RGB - light - TCS34725
   {0x00664624, "013"},  // Mic
-  {0x006648F0, "014"},  // Humidity
-  {0x00A91667, "015"},  // Humidity
+  {0x006648F0, "014"},  // Humidity DHT22
+  {0x00A91667, "015"},  // Humidity DHT22
   {0x0065AFF1, "016"},  // Light different then 012 [LIGHT - ISL29125] - ADC 16 bits - 65535
-  {0x006648F3, "017"},  // -- Humidity
-  {0x00664552, "018"},  // -- Humidity
-  {0x006643D4, "019"},  // -- air quality
-  {0x00664871, "020"},  // -- weight 4x50kg
+  {0x006648F3, "017"},  // Humidity DHT22
+  {0x00664552, "018"},  // Humidity DHT22
+  {0x00662689, "019"},  // weight single cell 1kg?
+  {0x00664871, "020"},  // weight 4x50kg
   {0x0022AD43, "021"},  // Mobile Gyro on battery (Wemos D1)
   {0x00D52499, "022"},  // Dust sensor
-  {0x00AAAAAA, "023"},   // Thermal camera Hi-Res  (ESP32) separate code
+  {0x00AAAAAA, "023"},  // Thermal camera Hi-Res 32x24 (ESP32) separate code
+  {0x006643D4, "024"},  // dummy - spare box
   {0x0042970A, "031"},  // Sand valve - prototype
   {0x00D5B7A1, "032"},  // Sand valve 2
   {0x00D5A22A, "033"},  // Water valve 1
